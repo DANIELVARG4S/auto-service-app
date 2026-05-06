7d60497458488386eba14324b5e13e203b6311a0
Orion <danniliho@gmail.com>
2026-05-05 20:55:27 -0600

    Mejora de APP vista

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import { Routes, Route, Link, Navigate } from "react-router-dom";
-import { Login } from "./pages/Login";
-import { Register } from "./pages/Register";
+import {Login} from "./pages/login/Login"
+import { Register } from "./pages/register/Register";
+import { Dashboard } from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      {/* Navbar */}
-      <nav className="bg-gray-800 p-4 text-white flex gap-4 mb-4">
-        <Link to="/login" className="hover:text-yellow-400">Login</Link>
-        <Link to="/register" className="hover:text-yellow-400">Register</Link>
-      </nav>
+    <Routes>
+      {/* Públicas */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-      <Routes>
-        {/* Redirige la raíz a /login */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </div>
+      {/* Privada */}
+      <Route path="/dashboard/*" element={<Dashboard />} />
+
+      {/* Redirección */}
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
   );
 }
 
