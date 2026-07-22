08f1e6e2f287f24fbecf7582936638a777a65c34
Orion <danniliho@gmail.com>
2026-07-21 22:54:15 -0600

    mejora en url

--- src/api/client.js
+++ src/api/client.js
@@ -8,31 +8,31 @@ const client = axios.create({
 });
 
 // 🔐 Interceptor para agregar el token automáticamente
-// client.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
+client.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
-// // ⚠️ Interceptor para manejar errores globales
-// client.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       // Token inválido o expirado
-//       localStorage.removeItem("token");
-//       window.location.href = "/login";
-//     }
+// ⚠️ Interceptor para manejar errores globales
+client.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // Token inválido o expirado
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
 
-//     return Promise.reject(error);
-//   }
-// );
+    return Promise.reject(error);
+  }
+);
 
 export default client;
