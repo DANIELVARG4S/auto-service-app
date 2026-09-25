f297a15042ee62412d9037fcd2364e5546f52622
Orion <danniliho@gmail.com>
2026-09-24 22:17:53 -0600

    agregando edicion enpont de vehiculos

--- src/pages/vehiculos/services/vehicleService.js
+++ src/pages/vehiculos/services/vehicleService.js
@@ -1,8 +1,13 @@
 import client from '../../../api/client';
 
+export const createVehicle = async (payload) => {
+    const response = await client.post('/vehicles', payload);
+    return response.data;
+};
+
 export const updateVehicle = async (id, payload) => {
     const response = await client.put(`/vehicles/${id}`, payload);
     return response.data;
 };
 
-export default { updateVehicle };
+export default { createVehicle, updateVehicle };
